70bd5e833f4f374512201077105e22821040d677
nazli-bucak <nazli.bucak@enuygun.com>
2024-06-26 16:56:13 +0300

    .

--- src/test/java/org/example/StepImplementation.java
+++ src/test/java/org/example/StepImplementation.java
@@ -128,6 +128,8 @@ public class StepImplementation {
                 clickElement("sonrakiAyaIlerle");
             }
             clickElement("sonSecilenGun");
+            clickElement("buttonUygula");
+            waitBySeconds(2);
         }
     }
 
